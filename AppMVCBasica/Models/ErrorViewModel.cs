7446f5030f9c94921579180c089c13fd9d7efabe
Thiago Corrêa da Fonseca <thiagocorreafonseca@gmail.com>
2020-10-10 23:42:41 -0300

    Add project files.

---
+++ AppMVCBasica/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppMVCBasica.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
